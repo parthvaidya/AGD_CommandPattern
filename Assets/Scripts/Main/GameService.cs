3a8151ca79a954a68516b352dee52c38b1c05ca3
Parth Vaidya <zeusparth99@gmail.com>
2025-03-26 14:21:42 +0530

    Created a Replay Service

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -8,6 +8,7 @@ using Command.UI;
 using Command.Events;
 using Command.Battle;
 using Command.Actions;
+using Command.Replay;
 using UnityEngine.UI;
 using Command.Commands;
 
@@ -29,6 +30,7 @@ namespace Command.Main
         public BattleService BattleService { get; private set; }
         public PlayerService PlayerService { get; private set; }
         public CommandInvoker CommandInvoker { get; private set; }
+        public ReplayService ReplayService { get; private set; }
 
         [SerializeField] private UIService uiService;
         public UIService UIService => uiService;
@@ -51,6 +53,7 @@ namespace Command.Main
             PlayerService = new PlayerService();
             uiService.Init(battleScriptableObjects.Count);
             CommandInvoker = new CommandInvoker();
+            ReplayService = new ReplayService();
         }
 
         private void Update() => InputService.UpdateInputService();
