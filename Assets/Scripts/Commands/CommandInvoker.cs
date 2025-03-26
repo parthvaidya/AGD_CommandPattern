2f4d891c505c87c6bf848ec96503f518aaf6ef0b
Parth Vaidya <zeusparth99@gmail.com>
2025-03-26 11:57:23 +0530

    Updated Command Invoker

--- Assets/Scripts/Commands/CommandInvoker.cs
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -1,3 +1,4 @@
+using Command.Main;
 using System.Collections.Generic;
 
 namespace Command.Commands
@@ -12,8 +13,18 @@ namespace Command.Commands
             RegisterCommand(commandToProcess);
         }
 
+        public void Undo()
+        {
+            if (!RegistryEmpty() && CommandBelongsToActivePlayer())
+                commandRegistry.Pop().Undo();
+        }
+
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
 
         public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+
+        private bool RegistryEmpty() => commandRegistry.Count == 0;
+        private bool CommandBelongsToActivePlayer() => (commandRegistry.Peek() as UnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
+
     }
 }
